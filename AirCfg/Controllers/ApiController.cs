93a224036a45b59089ccc8647fff71ee1db87894
abdusselam.kosecik <abdusselam.kosecik@sys.com.tr>
2021-02-20 14:11:07 +0300

    First Commit

---
+++ AirCfg/Controllers/ApiController.cs
@@ -0,0 +1,72 @@
+﻿using Ist.Pir.AirCfg.Infrastructure;
+using Ist.Pir.AirCfg.Models;
+using LiteDB;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ist.Pir.AirCfg.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        #region Private Fields
+
+        private readonly string location = "";
+        private readonly string startString = "_airCfg_Pirsoft_";
+        private readonly IEncryption _encryption;
+
+        #endregion Private Fields
+
+        #region Public Methods
+        public ApiController(IWebHostEnvironment env, IEncryption encryption)
+        {
+            _encryption = encryption;
+            location = "Filename=" + Path.Combine(env.WebRootPath, "data", "data.db") + ";connection=shared";
+        }
+
+        // POST api/<ApiController>
+        [HttpPost]
+        public string Post([FromBody] SearchModel value)
+        {
+            using (var db = new LiteDatabase(location))
+            {
+                var records = db.GetCollection<Entity>("records");
+                Entity recordModel = null;
+                try
+                {
+                    recordModel = records.FindById(new BsonValue(new ObjectId(value.Id)));
+                }
+                catch
+                {
+
+                }
+
+                if (recordModel != null)
+                {
+                    var data = _encryption.DecryptString(recordModel.Data, value.Key);
+                    if (!data.StartsWith(startString))
+                    {
+                        return "Wrong password";
+                    }
+                    recordModel.LastRequest = DateTime.Now;
+                    records.Update(recordModel);
+                    return data;
+                }
+                else
+                {
+                    return "Not found";
+                }
+            }
+        }
+    }
+    #endregion
+}
+
---
+++ AirCfg/Models/Entity.cs
@@ -0,0 +1,18 @@
+﻿using LiteDB;
+using System;
+
+namespace Ist.Pir.AirCfg.Models
+{
+    public class Entity
+    {
+        #region Public Properties
+
+        public DateTime CreateTime { get; set; }
+        public string Data { get; set; }
+        public ObjectId Id { get; set; } 
+        public DateTime LastRequest { get; set; }
+        public DateTime UpdateTime { get; set; }
+
+        #endregion Public Properties
+    }
+}
